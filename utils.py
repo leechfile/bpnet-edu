3dd47a4dc82766d86704d50caa136964b799bcb6
leechfile <2544048170@qq.com>
2023-11-21 09:15:36 +0800

    add ui note

--- utils.py
+++ utils.py
@@ -3,46 +3,54 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-class SimpleNeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size):
-        # 初始化网络结构
+class SimpleNeuralNetworkModified:
+    def __init__(self, input_size, hidden_size, output_size, learning_rate=0.1, epochs=10000):
+        # 初始化网络结构和超参数
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
 
         # 初始化权重
         self.W1 = np.random.randn(self.input_size, self.hidden_size)
         self.W2 = np.random.randn(self.hidden_size, self.output_size)
 
     def sigmoid(self, z):
+        # Sigmoid激活函数
         return 1 / (1 + np.exp(-z))
 
     def sigmoid_derivative(self, z):
+        # Sigmoid函数的导数
         return z * (1 - z)
 
     def forward(self, X):
-        # 前向传播
-        self.z2 = np.dot(X, self.W1)
-        self.a2 = self.sigmoid(self.z2)
-        self.z3 = np.dot(self.a2, self.W2)
-        y_hat = self.sigmoid(self.z3)
+        # 前向传播过程
+        self.z2 = np.dot(X, self.W1)  # 隐藏层输入
+        self.a2 = self.sigmoid(self.z2)  # 隐藏层激活输出
+        self.z3 = np.dot(self.a2, self.W2)  # 输出层输入
+        y_hat = self.sigmoid(self.z3)  # 输出层激活输出
         return y_hat
 
-    def backward(self, X, y, y_hat, learning_rate):
+    def backward(self, X, y, y_hat):
         # 反向传播和权重更新
+        # 计算输出层误差
         self.error = y - y_hat
+        # 应用激活函数的导数计算输出层梯度
         self.delta3 = self.error * self.sigmoid_derivative(y_hat)
+        # 计算隐藏层的误差
         self.z2_error = self.delta3.dot(self.W2.T)
+        # 应用激活函数的导数计算隐藏层梯度
         self.delta2 = self.z2_error * self.sigmoid_derivative(self.a2)
 
         # 更新权重
-        self.W1 += X.T.dot(self.delta2) * learning_rate
-        self.W2 += self.a2.T.dot(self.delta3) * learning_rate
+        self.W1 += X.T.dot(self.delta2) * self.learning_rate
+        self.W2 += self.a2.T.dot(self.delta3) * self.learning_rate
 
-    def train(self, X, y, learning_rate=0.1, epochs=10000):
-        for _ in range(epochs):
+    def train(self, X, y):
+        for _ in range(self.epochs):
             y_hat = self.forward(X)
-            self.backward(X, y, y_hat, learning_rate)
+            self.backward(X, y, y_hat)
 
     def predict(self, X):
         # 预测新数据的函数
@@ -51,6 +59,9 @@ class SimpleNeuralNetwork:
     def get_weights(self):
         # 返回各层的权重
         return self.W1, self.W2
+
+
+
 
 
 def preprocess_data(data):
@@ -113,24 +124,16 @@ if __name__ == '__main__':
     # 可视化预处理后的数据
     visualize_data(preprocessed_data)
 
+    # 创建一个修改后的网络实例
+    nn_modified = SimpleNeuralNetworkModified(input_size=3, hidden_size=4, output_size=1, learning_rate=0.01,
+                                              epochs=1000)
 
-    # 创建一个网络实例
-    nn = SimpleNeuralNetwork(input_size=3, hidden_size=4, output_size=1)
-
-    # 测试数据
-    X_train = np.array([[0, 0, 1],
-                        [0, 1, 1],
-                        [1, 0, 1],
-                        [1, 1, 1]])
-    y_train = np.array([[0],
-                        [1],
-                        [1],
-                        [0]])
-
-    # 训练网络
-    nn.train(X_train, y_train)
+    # 使用同样的测试数据
+    nn_modified.train(X_train, y_train)
 
     # 测试网络的预测功能
-    predictions = nn.predict(X_train)
-    weights = nn.get_weights()
-    print(predictions, weights)  # 返回预测结果和网络的权重
+    predictions_modified = nn_modified.predict(X_train)
+    weights_modified = nn_modified.get_weights()
+
+    print(predictions_modified, weights_modified ) # 返回预测结果和网络的权重
+
